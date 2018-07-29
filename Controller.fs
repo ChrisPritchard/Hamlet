8b1313aafde3344c2525acbab0edf943ad85d567
Christopher <chrispritchard.nz@gmail.com>
2018-07-30 11:41:47 +1200

    Escape key for exit

--- Controller.fs
+++ Controller.fs
@@ -2,8 +2,10 @@ module Controller
 
 open GameCore
 open Model
+open Microsoft.Xna.Framework.Input
 
 let advanceGame (runState : RunState) =
     function
+    | _ when runState.WasJustPressed Keys.Escape -> None
     | None -> Some ({ tiles = [] })
     | Some w -> Some w
